081ec0818aa5eb215cffc00e598d175bf592aca5
Steve McDougall <juststevemcd@gmail.com>
2025-01-05 22:58:36 +0000

    chore: tidying up routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,23 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
+return Application::configure(
+    basePath: dirname(__DIR__),
+)->withRouting(
+    web: __DIR__ . '/../routes/web/routes.php',
+    commands: __DIR__ . '/../routes/console/routes.php',
+    health: '/up',
+)->withMiddleware(
+    callback: function (Middleware $middleware): void {
         $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            HandleInertiaRequests::class,
+            AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    },
+)->withExceptions(
+    using: function (Exceptions $exceptions): void {},
+)->create();
